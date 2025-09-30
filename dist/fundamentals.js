2a9b8bd5d5657316b8a7b9ed2c03b0e4f1befcea
Havy <hnakalyowa29@gmail.com>
2025-09-30 22:51:48 +0300

    i ran my file

--- dist/fundamentals.js
+++ dist/fundamentals.js
@@ -1,22 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 let sales = 20_000_000;
 let course = 'TypeScript';
 let is_published = true;
 let level; //any
 level = 1;
 level = 'a';
-
-
-function render(document: any) {
+function render(document) {
     console.log(document);
 }
-
 //Arrays
 //let numbers: number[] = [1, 2, '3']; //string not allowed
 let numbers2 = [1, 2, 3];
 //code completion should suggest number methods
 ///numbers.forEach(n => n)
-
 //Tuples
 //let user: [number, string] = [1, 'Mosh',0];//3rd element not allowed
-let user1: [number, string] = [1, 'Mosh'];
-
+let user1 = [1, 'Mosh'];
+//# sourceMappingURL=fundamentals.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello, World!");
 let age = 25;
+if (age < 50)
+    age += 10;
 //# sourceMappingURL=index.js.map
