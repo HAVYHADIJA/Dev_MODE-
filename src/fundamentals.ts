c8b20259fa2baf520a778fb5fe0b23bf39639996
Havy <hnakalyowa29@gmail.com>
2025-09-30 22:47:08 +0300

    new additions

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -13,3 +13,10 @@ function render(document: any) {
 //Arrays
 let numbers: number[] = [1, 2, '3']; //string not allowed
 let numbers2 = [1, 2, 3];
+//code completion should suggest number methods
+numbers.forEach(n => n)
+
+//Tuples
+let user: [number, string] = [1, 'Mosh',0];//3rd element not allowed
+let user1: [number, string] = [1, 'Mosh'];
+
