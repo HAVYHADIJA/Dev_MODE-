26b412b6a70b5041aa31e0cb2b92e1ea1f7a85d7
Havy <hnakalyowa29@gmail.com>
2025-09-30 22:41:15 +0300

    changes

--- src/fundamentals.ts
+++ src/fundamentals.ts
@@ -1,4 +1,15 @@
 let sales = 20_000_000;
 let course = 'TypeScript';
 let is_published = true;
-let level;
+let level; //any
+level = 1;
+level = 'a';
+
+
+function render(document: any) {
+    console.log(document);
+}
+
+//Arrays
+let numbers: number[] = [1, 2, '3']; //string not allowed
+let numbers2 = [1, 2, 3];
